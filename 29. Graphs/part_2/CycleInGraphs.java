a7bcbaa05a015ae917355d6ab707964770d96bb0
sachiraghab <sachiraghab@gmail.com>
2024-07-08 10:52:34 +0530

    Graphs added

---
+++ 29. Graphs/part_2/CycleInGraphs.java
@@ -0,0 +1,82 @@
+/*
+Q. Cycle in graphs:
+*/
+import java.util.*;
+public class CycleInGraphs{
+  static class Edge{
+    int src, dest;
+    public Edge(int s, int d){
+      this.src = s;
+      this.dest = d;
+    }
+  }
+  static void createGraph(ArrayList<Edge> graph[]){
+    for(int i=0; i<graph.length; i++){
+      graph[i] = new ArrayList<>();
+    }
+    graph[0].add(new Edge(0,1));
+    graph[0].add(new Edge(0,2));
+    graph[0].add(new Edge(0,3));
+
+    graph[1].add(new Edge(1,0));
+    graph[1].add(new Edge(1,2));
+
+    graph[2].add(new Edge(2,0));
+    graph[2].add(new Edge(2,1));
+
+    graph[3].add(new Edge(3,0));
+    graph[3].add(new Edge(3,4));
+
+    graph[4].add(new Edge(4,3));
+  }
+  public static boolean detectCycle(ArrayList<Edge> graph[]){
+    boolean vis[] = new boolean[graph.length];
+    for(int i=0; i<graph.length; i++){
+      if(!vis[i]){
+        if(detectCycleUtil(graph,vis,i,-1)){
+          return true;
+          //cycle exists in one of the parts
+        }
+      }
+    }
+    return false;
+  }
+  public static boolean detectCycleUtil(ArrayList<Edge> graph[], boolean vis[], int curr, int par){
+    vis[curr] = true;
+    for(int i=0; i<graph[curr].size(); i++){
+      Edge e = graph[curr].get(i);
+      //case 3
+      if(!vis[e.dest]){
+        if(detectCycleUtil(graph,vis,e.dest,curr)){
+          return true;
+        }
+      }
+      //case 1
+      else if(vis[e.dest] && e.dest != par){
+        return true;
+      }
+      //case 2 -> do nothing -> continue
+    }
+    return false;
+  }
+  public static void dfs(ArrayList<Edge>[] graph, int curr, boolean vis[]){
+    //visit
+    System.out.print(curr+" ");
+    vis[curr] = true;
+    for(int i=0; i<graph[curr].size(); i++){
+      Edge e = graph[curr].get(i);
+      if(!vis[e.dest]){
+        dfs(graph, e.dest, vis);
+      }
+    }
+  }
+  public static void main(String[] args){
+    int V = 7;
+    ArrayList<Edge>[] graph = new ArrayList[V]; //null -> empty arraylist
+    createGraph(graph);
+    dfs(graph, 0, new boolean[V]);
+    System.out.println(detectCycle(graph));
+  }
+}
+
+// java CycleInGraphs.java
